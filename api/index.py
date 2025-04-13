ac63d5c9f76dbeba13b5403154464631c9c0beec
urbajoe <urbajoe@gmail.com>
2025-04-13 14:25:17 -0300

    Adiciona rota de edição e corrige conexão com Neon

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
 from app import app
 
-# Expor a aplicação Flask como handler do Vercel
-def handler(req, res):
-    return app(req, res)
+def handler(request, response):
+    return app(request, response)
